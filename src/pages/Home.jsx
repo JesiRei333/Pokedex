403d9abd539eec10c3d759f7252092c56c180e7a
Jesica Reyes <miucomosmos333@gmail.com>
2024-03-28 23:08:41 -0600

    add justificado center nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Outlet, Link } from "react-router-dom";
 export default function Home() {
   return (
     <main className="bg-slate-950 min-h-screen">
-      <nav className=" fixed flex sm:text-sm sm:justify-center items-center  font-semibold w-full md:gap-12  bg-white/20 md:text-xl md:justify-around md:p-1.5 md:pl-60 md:pr-60">
+      <nav className=" fixed flex sm:text-sm justify-center items-center  font-semibold w-full md:gap-12  bg-white/20 md:text-xl md:justify-around md:p-1.5 md:pl-60 md:pr-60">
         <Link className="  md:pl-3" to="/">
           <div className="flex justify-center items-center   ">
             <img
@@ -21,7 +21,7 @@ export default function Home() {
               src="    https://cdn-icons-png.flaticon.com/512/287/287221.png "
               alt="pokebola"
             />
-            <p className=" hover:font-bold pl-2">Pokédex</p>
+            <p className=" hover:font-bold pl-1">Pokédex</p>
           </div>
         </Link>
 
@@ -36,7 +36,7 @@ export default function Home() {
               />
             </div>
             <div className="flex justify-center items-center ">
-              <p className=" hover:font-bold pl-2">PokeGacha </p>
+              <p className=" hover:font-bold pl-1">PokeGacha </p>
             </div>
           </div>
         </Link>
@@ -52,7 +52,7 @@ export default function Home() {
               />
             </div>
             <div className="flex justify-center items-center ">
-              <p className=" hover:font-bold pl-2">Poke-inicial</p>
+              <p className=" hover:font-bold pl-1">Poke-inicial</p>
             </div>
           </div>
         </Link>
