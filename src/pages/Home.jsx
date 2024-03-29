7836685e0cde4cd62c7587005420d0cd9deb1a44
Jesica Reyes <miucomosmos333@gmail.com>
2024-03-28 22:54:07 -0600

    add tamaño

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Outlet, Link } from "react-router-dom";
 export default function Home() {
   return (
     <main className="bg-slate-950 min-h-screen">
-      <nav className="  fixed flex sm:justify-center items-center  text-base font-semibold w-full md:gap-12  bg-white/20 md:text-xl md:justify-around md:p-1.5 md:pl-60 md:pr-60">
+      <nav className="  fixed flex sm:text-sm sm:justify-center items-center  font-semibold w-full md:gap-12  bg-white/20 md:text-xl md:justify-around md:p-1.5 md:pl-60 md:pr-60">
         <Link className=" md:pl-3" to="/">
           <div className="flex justify-center items-center   ">
             <img
